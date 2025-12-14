a170a56b13ad1cf59425d94ef5e089bbb131c0f4
exceptional007 <akshhatsri7843@gmail.com>
2025-12-14 11:25:53 +0530

    Basic React App

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import ProductList from "./components/ProductList";
+
+function App() {
+  return (
+    <div className="container">
+      <h1>Product List</h1>
+      <ProductList />
+    </div>
+  );
+}
+
+export default App;
